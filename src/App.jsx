5c5afce9ae6cc6819f03a07f97817013e3f52db3
Gowthamstack <gowthamsamu2003@gmail.com>
2026-04-17 14:12:24 +0530

    Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="bg-black flex items-center flex-col text-white">
       <Navbar />
-      <Hero />
+      <Hero/>
       <About />
       <Projects />
       <Contact />
